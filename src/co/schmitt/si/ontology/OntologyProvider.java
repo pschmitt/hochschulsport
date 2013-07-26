5807cb8c1acb6e73044901d39ee6cd354f3abbaf
Sophia Chatzimagas <sophia.chatzimagas@gmail.com>
2013-07-26 12:24:56 +0200

    Changed package structure

--- src/co/schmitt/si/ontology/OntologyProvider.java
+++ src/co/schmitt/si/ontology/OntologyProvider.java
@@ -1,8 +1,8 @@
 package co.schmitt.si.ontology;
 
-import co.schmitt.si.ontology.model.Location;
-import co.schmitt.si.ontology.model.Sport;
-import co.schmitt.si.ontology.model.SportCategory;
+import co.schmitt.si.model.Location;
+import co.schmitt.si.model.Sport;
+import co.schmitt.si.model.SportCategory;
 
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
