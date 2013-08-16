be92e6a431153ad61f6dabd0f2447f36d5820f4c
Philipp Schmitt <philipp@schmitt.co>
2013-08-16 18:24:42 +0200

    Fixed: Wrong string conversion between ontology and db

--- src/co/schmitt/si/ontology/OntologyString.java
+++ src/co/schmitt/si/ontology/OntologyString.java
@@ -10,6 +10,6 @@ public class OntologyString {
 
     public static String convert(String className) {
         //        return className.replaceAll("ä", "ae").replaceAll("ö", "oe").replaceAll("ü", "ue").replaceAll("Ä", "Ae").replaceAll("Ö", "Oe").replaceAll("Ü", "Ue").replaceAll("ß", "ss").replaceAll(" ", "_");
-        return className.replaceAll(" ", "_");
+        return className.replaceAll("_", " ");
     }
 }
